42e09593916486c4198dbcc0d3969e257f8e32af
Emmanuel Essien <xxcel4ryll@gmail.com>
2024-04-04 18:52:00 +0100

    deployment resolved

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminRepository } from './admin.repository';
 import Admin from './admin.entity';
 import { AdminService } from './admin.service';
-import { SettingService } from '../Setting/setting.service';
-import { SettingRepository } from '../Setting/setting.repository';
-import Setting from '../Setting/setting.entity';
+import { SettingService } from '../setting/setting.service';
+import { SettingRepository } from '../setting/setting.repository';
+import Setting from '../setting/setting.entity';
 import { AdminController } from './admin.controller';
 
 @Module({
