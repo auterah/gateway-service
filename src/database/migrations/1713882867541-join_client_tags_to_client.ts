4b6b8945946b0a69014e996420e3c6f5130f39aa
God'sgift Uko <godsgiftuko@gmail.com>
2024-04-23 19:10:01 +0100

    Remove Drop tags column from JoinClientTagsToClient1713882867541

--- src/database/migrations/1713882867541-join_client_tags_to_client.ts
+++ src/database/migrations/1713882867541-join_client_tags_to_client.ts
@@ -5,9 +5,6 @@ export class JoinClientTagsToClient1713882867541 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`customer_clients\` DROP COLUMN \`tags\``,
-    );
-    await queryRunner.query(
       `ALTER TABLE \`client_tags\` DROP FOREIGN KEY \`FK_752a22b36bfb68184764c845c11\``,
     );
     await queryRunner.query(
@@ -33,9 +30,6 @@ export class JoinClientTagsToClient1713882867541 implements MigrationInterface {
     );
     await queryRunner.query(
       `ALTER TABLE \`client_tags\` ADD CONSTRAINT \`FK_752a22b36bfb68184764c845c11\` FOREIGN KEY (\`customer_id\`) REFERENCES \`customers\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`customer_clients\` ADD \`tags\` json NOT NULL`,
     );
   }
 }
