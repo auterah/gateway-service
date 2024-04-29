afed4ae5576d765d904d83bf570284efdf3665c8
God'sgift Uko <godsgiftuko@gmail.com>
2024-04-29 23:31:32 +0100

    Update setting controller

--- src/modules/customer/controllers/customer_settings.controller.ts
+++ src/modules/customer/controllers/customer_settings.controller.ts
@@ -12,18 +12,18 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { FindDataRequestDto } from 'src/shared/utils/dtos/find.data.request.dto';
-import { AdminGuard } from '../../auth/guards/admin_guard';
 import { CustomerSettingsService } from '../services/customer_settings.service';
 import { GetCurrentCustomer } from 'src/shared/decorators/get_current_customer';
 import { CustomerSettingDto } from '../dtos/customer_settings.dto';
 import Customer from '../entities/customer.entity';
 import { ActionsGuard } from 'src/modules/auth/guards/actions_guard';
+import { AdminGuard } from 'src/modules/auth/guards/admin_guard';
 
 @Controller('customers/settings')
 export class CustomerSettingsController {
   constructor(private customerSettingsService: CustomerSettingsService) {}
 
-  @Get()
+  @Get('all')
   @UseGuards(AdminGuard)
   getCustomerSettings(
     @Query() queries: FindDataRequestDto,
@@ -36,7 +36,7 @@ export class CustomerSettingsController {
   }
 
   @Put()
-  @UseGuards(AdminGuard)
+  @UseGuards(ActionsGuard)
   updateCustomerSettings(
     @Body() updates: CustomerSettingDto,
     @GetCurrentCustomer() customer: Customer,
